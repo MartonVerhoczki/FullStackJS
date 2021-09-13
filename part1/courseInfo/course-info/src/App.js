8bea51e52bf6a7def895edfba5d71929a4de875d
MartonVerhoczki <marton.verhoczki@instructure.com>
2021-09-13 13:22:06 +0200

    Exercises 1.3-1.5

--- part1/courseInfo/course-info/src/App.js
+++ part1/courseInfo/course-info/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.courseName}</h1>;
 };
 
 const Content = (props) => {
+  console.log(props.parts);
   return props.parts.map((part) => (
-    <Part title={part.title} numberOfExcercises={part.numberOfExcercises} />
+    <Part name={part.name} exercises={part.exercises} />
   ));
 };
 
@@ -17,31 +18,37 @@ const Total = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.title} {props.numberOfExcercises}
+      {props.name} {props.exercises}
     </p>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        parts={[
-          { title: part1, numberOfExcercises: exercises1 },
-          { title: part2, numberOfExcercises: exercises2 },
-          { title: part3, numberOfExcercises: exercises3 },
-        ]}
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        sum={course.parts.map((a) => a.exercises).reduce((a, b) => a + b)}
       />
-      <Total sum={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };

40ceedc1a1d7d982a5706d788edfbab1c3d2a4b8
MartonVerhoczki <marton.verhoczki@instructure.com>
2021-09-15 15:00:58 +0200

    Excercises 2.6-2.10 (Fixed version, move component definitions outside of App component

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,65 @@
 import React, { useState } from "react";
 
+const Filter = ({ filter, setFilter }) => {
+  const handleFilter = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      Filter shown with: <input value={filter} onChange={handleFilter} />
+    </div>
+  );
+};
+
+const PersonForm = ({
+  persons,
+  setPersons,
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+}) => {
+  const handleNewPerson = (event) => {
+    event.preventDefault();
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      let copy = [...persons];
+      copy.push({ name: newName, number: newNumber });
+      setPersons(copy);
+    }
+  };
+
+  return (
+    <form onSubmit={handleNewPerson}>
+      <label>
+        Name:
+        <input onChange={(event) => setNewName(event.target.value)} />
+      </label>
+      <label>
+        Number:
+        <input onChange={(event) => setNewNumber(event.target.value)} />
+      </label>
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+const Persons = ({ persons, filter }) => {
+  return persons
+    .filter((person) =>
+      person.name.toLowerCase().includes(filter.toLowerCase())
+    )
+    .map((person) => (
+      <div key={person.id}>
+        <p>
+          {person.name} - {person.number}
+        </p>
+      </div>
+    ));
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
@@ -11,67 +71,21 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
 
-  const Filter = () => {
-    const handleFilter = (event) => {
-      setFilter(event.target.value);
-    };
-
-    return (
-      <div>
-        Filter shown with: <input value={filter} onChange={handleFilter} />
-      </div>
-    );
-  };
-
-  const PersonForm = () => {
-    const handleNewPerson = (event) => {
-      event.preventDefault();
-      if (persons.map((person) => person.name).includes(newName)) {
-        alert(`${newName} is already added to phonebook`);
-      } else {
-        let copy = [...persons];
-        copy.push({ name: newName, number: newNumber });
-        setPersons(copy);
-      }
-    };
-
-    return (
-      <form onSubmit={handleNewPerson}>
-        <label>
-          Name:
-          <input onChange={(event) => setNewName(event.target.value)} />
-        </label>
-        <label>
-          Number:
-          <input onChange={(event) => setNewNumber(event.target.value)} />
-        </label>
-        <button type="submit">Add</button>
-      </form>
-    );
-  };
-
-  const Persons = () => {
-    return persons
-      .filter((person) =>
-        person.name.toLowerCase().includes(filter.toLowerCase())
-      )
-      .map((person) => (
-        <div key={person.id}>
-          <p>
-            {person.name} - {person.number}
-          </p>
-        </div>
-      ));
-  };
-
   return (
     <>
       <h2>Phonebook</h2>
-      <Filter />
-      <PersonForm />
+      <Filter filter={filter} setFilter={setFilter} />
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
 
       <h2>Numbers</h2>
-      <Persons />
+      <Persons persons={persons} filter={filter} />
     </>
   );
 };
